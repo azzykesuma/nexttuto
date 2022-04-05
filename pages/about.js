2fb17f4315655bfa388ccb71406f97648604a187
azzy kesuma <official.azzy@gmail.com>
2022-04-05 08:28:47 +0700

    first commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde et ullam, quidem consequatur dolore cumque sequi, quae quia eaque obcaecati quis perspiciatis reprehenderit eligendi optio officiis. Enim explicabo nobis quo?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde et ullam, quidem consequatur dolore cumque sequi, quae quia eaque obcaecati quis perspiciatis reprehenderit eligendi optio officiis. Enim explicabo nobis quo?</p>
+        </div>
+    );
+}
+ 
+export default About;
---
+++ pages/ninja/index.js
@@ -0,0 +1,9 @@
+const NinjaList = () => {
+    return (
+        <div>
+            <h1>Ninja List</h1>
+        </div>
+    );
+}
+ 
+export default NinjaList;
